7eb2f1276484ac5b38efd9b857bf3b828f5da808
Gordon Gao <d.gao@student.vu.nl>
2024-06-06 12:16:15 +0200

    updated signal loop example

--- train.ino
+++ train.ino
@@ -48,6 +48,7 @@ Adafruit_LiquidCrystal lcd(0);
 
 
 MCP23017 mcp = MCP23017(0x26);
+MCP23017 mcp = MCP23017(0x27);
 
  // TRAIN init
 #define DATAPIN 16
@@ -102,6 +103,15 @@ void setup() {
   digitalWrite(DATAPIN, LOW);
   pinMode(ENABLEPIN, OUTPUT);
   digitalWrite(ENABLEPIN, HIGH);
+
+
+//sign signal setup
+  Wire.begin();
+  mcp.init();
+  mcp.writeRegister(MCP23017Register::IODIR_A, (unsigned char )0x00); // set IO direction to output
+  mcp.writeRegister(MCP23017Register::IODIR_B, (unsigned char )0x00);
+  mcp.writeRegister(MCP23017Register::OLAT_A, (unsigned char )0b11111111);
+  mcp.writeRegister(MCP23017Register::OLAT_B, (unsigned char )0b11111111);
 }
 
 void loop() {
@@ -109,8 +119,87 @@ void loop() {
   LED_exp();
 
   switch_loop_exp();
+  // signal_loop_exp();
 }
 
+void signal_loop_exp()
+{
+  uint16_t idx;
+  uint16_t signals;
+  for (idx=0; idx<16; idx++) {
+  switch(idx) {
+  case 0:
+    mcp.writeRegister(MCP23017Register::OLAT_A, (unsigned char )0b11111110);
+    mcp.writeRegister(MCP23017Register::OLAT_B, (unsigned char )0b11111111);
+  break;
+  case 1:
+    mcp.writeRegister(MCP23017Register::OLAT_A, (unsigned char )0b11111101);
+    mcp.writeRegister(MCP23017Register::OLAT_B, (unsigned char )0b11111111);
+  break;
+  case 2:
+    mcp.writeRegister(MCP23017Register::OLAT_A, (unsigned char )0b11111011);
+    mcp.writeRegister(MCP23017Register::OLAT_B, (unsigned char )0b11111111);
+  break;
+  case 3:
+    mcp.writeRegister(MCP23017Register::OLAT_A, (unsigned char )0b11110111);
+    mcp.writeRegister(MCP23017Register::OLAT_B, (unsigned char )0b11111111);
+  break;
+  case 4:
+    mcp.writeRegister(MCP23017Register::OLAT_A, (unsigned char )0b11101111);
+    mcp.writeRegister(MCP23017Register::OLAT_B, (unsigned char )0b11111111);
+  break;
+  case 5:
+    mcp.writeRegister(MCP23017Register::OLAT_A, (unsigned char )0b11011111);
+    mcp.writeRegister(MCP23017Register::OLAT_B, (unsigned char )0b11111111);
+  break;
+  case 6:
+    mcp.writeRegister(MCP23017Register::OLAT_A, (unsigned char )0b10111111);
+    mcp.writeRegister(MCP23017Register::OLAT_B, (unsigned char )0b11111111);
+  break;
+  case 7:
+    mcp.writeRegister(MCP23017Register::OLAT_A, (unsigned char )0b01111111);
+    mcp.writeRegister(MCP23017Register::OLAT_B, (unsigned char )0b11111111);
+  break;
+  case 8:
+    mcp.writeRegister(MCP23017Register::OLAT_A, (unsigned char )0b11111111);
+    mcp.writeRegister(MCP23017Register::OLAT_B, (unsigned char )0b11111110);
+  break;
+  case 9:
+    mcp.writeRegister(MCP23017Register::OLAT_A, (unsigned char )0b11111111);
+    mcp.writeRegister(MCP23017Register::OLAT_B, (unsigned char )0b11111101);
+  break;
+  case 10:
+    mcp.writeRegister(MCP23017Register::OLAT_A, (unsigned char )0b11111111);
+    mcp.writeRegister(MCP23017Register::OLAT_B, (unsigned char )0b11111011);
+  break;
+  case 11:
+    mcp.writeRegister(MCP23017Register::OLAT_A, (unsigned char )0b11111111);
+    mcp.writeRegister(MCP23017Register::OLAT_B, (unsigned char )0b11110111);
+  break;
+  case 12:
+    mcp.writeRegister(MCP23017Register::OLAT_A, (unsigned char )0b11111111);
+    mcp.writeRegister(MCP23017Register::OLAT_B, (unsigned char )0b11101111);
+  break;
+  case 13:
+    mcp.writeRegister(MCP23017Register::OLAT_A, (unsigned char )0b11111111);
+    mcp.writeRegister(MCP23017Register::OLAT_B, (unsigned char )0b11011111);
+  break;
+  case 14:
+    mcp.writeRegister(MCP23017Register::OLAT_A, (unsigned char )0b11111111);
+    mcp.writeRegister(MCP23017Register::OLAT_B, (unsigned char )0b10111111);
+  break;
+  case 15:
+    mcp.writeRegister(MCP23017Register::OLAT_A, (unsigned char )0b11111111);
+    mcp.writeRegister(MCP23017Register::OLAT_B, (unsigned char )0b01111111);
+  break;
+  default:
+  break;
+  }
+  delay(250);
+  }
+  mcp.writeRegister(MCP23017Register::OLAT_A, (unsigned char )0b11111111);
+  mcp.writeRegister(MCP23017Register::OLAT_B, (unsigned char )0b11111111);
+}
 void LED_exp()
 {
  // LED loop
